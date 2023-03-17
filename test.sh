cf2fe75ba2435f172f2bbf6c74071cd9f88ebbb3
r-mutax <magicspell.max.mix.mutax@gmail.com>
2023-03-17 23:00:52 +0900

    Add test script.

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./zig-out/bin/zcc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 "0"
+assert 42 "42"
